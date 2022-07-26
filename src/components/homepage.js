f91a6c239c66a366b996384d4d55bbb2a7d228eb
Prathamesh Rathi <prathameshrathi14@gmail.com>
2022-07-26 21:18:43 +0530

    Initial COmmit

---
+++ src/components/homepage.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { TextField, Typography, Button } from "@material-ui/core";
+
+function Homepage() {
+  const [value, setvalue] = useState("");
+  return (
+    <div style={{backgroundImage: `url("https://www.theindianwire.com/wp-content/uploads/2020/04/Flipkart.png")`,backgroundSize:"cover",height:"100vh"}}>
+    <div>
+      <Typography variant="h1" component="h2" style={{ textAlign: "center" }}>
+        OSS Inspector
+      </Typography>
+      <div style={{textAlign:"center",margin:"30px"}}>
+      <TextField
+        style = {{width: '600px', textAlign:'center'}}
+        id="outlined-basic"
+        label="URL"
+        variant="outlined"
+        value={value}
+        onChange={(val) => {
+          setvalue(val.target.value);
+          // console.log(val.target.value);
+        }}
+      />
+      <Button variant="contained" color="primary" style={{marginLeft:"40px",marginTop:"8px"}}>Contained</Button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/github-score.js
@@ -0,0 +1,60 @@
+const constants = require("./constants");
+
+//We need to define max threshold values for all factors
+//Return Paramter Score given its current value,max value, and paramter wieght
+const getParamScore = (param,maxValue,weight=1) => {    
+    const score = (Math.log(1+param) / Math.log(1+Math.max(param,maxValue))) * weight;
+    return score;
+}
+
+const getRepositoryScore = (repo,additionalParams=null) => {
+
+    const additionalParamScore = 0;
+    const criticalityScore = 0;
+    const totalWeight = constants.FORKS_WEIGHT+constants.STARS_WEIGHT+constants.CLOSEDISSUE_WEIGHT+constants.CONTRIBUTORS_WEIGHT+constants.COMMITFREQUENCY_WEIGHT;
+
+    // additionalParams.map((additionalParam)=>{
+    //     additionalParamScore += getParamScore(additionalParam.value,additionalParam.max_threshold,additionalParam.weight);
+    // })   
+
+    //Validating
+    if(additionalParams.forks==null){
+        additionalParams.forks=0;
+    } 
+    if(additionalParams.stars==null){ 
+        additionalParams.stars =0;
+    }
+    if(additionalParams.closedIssues==null){
+        additionalParams.closedIssues=0;
+    }
+    if(additionalParams.pullRequestContributors==null){
+        additionalParams.pullRequestContributors=0;
+    }
+    if(additionalParams.commitCount4W==null){
+        additionalParams.commitCount4W=0;
+    }
+
+    criticalityScore = parseFloat((
+        (getParamScore(
+            additionalParams.forks,constants.FORKS_THRESHOLD,constants.FORKS_WEIGHT
+        )) +
+        (getParamScore(
+            additionalParams.stars,constants.STARS_THRESHOLD,constants.STARS_WEIGHT
+        )) +
+        (getParamScore(
+            additionalParams.closedIssues,constants.CLOSEDISSUE_THRESHOLD,constants.CLOSEDISSUE_WEIGHT
+        )) +
+        (getParamScore(
+            additionalParams.pullRequestContributors,constants.CONTRIBUTORS_THRESHOLD,constants.CONTRIBUTORS_WEIGHT
+        )) +
+        (getParamScore(
+            additionalParams.commitCount4W, constants.COMMITFREQUENCY_THRESHOLD,constants.COMMITFREQUENCY_WEIGHT
+        )) + additionalParamScore)  / totalWeight).toFixed(5);
+        
+        
+        criticalityScore = Math.max(Math.min(criticalityScore,1),0);
+        return criticalityScore;
+
+}
+
+module.exports = getRepositoryScore;
